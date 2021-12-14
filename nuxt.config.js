e2ffa224121105ec6005a880191f7bfd22e2f6e7
JeanDevFR <castellotti.j@gmail.com>
2021-12-14 21:31:54 +0100

    Update Nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,29 +1,29 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
-  target: "static",
+  target: 'static',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "Charles Cantin - Photographe",
+    title: 'Charles Cantin - Photographe',
     htmlAttrs: {
-      lang: "fr",
+      lang: 'fr',
     },
     meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
-        hid: "description",
-        name: "description",
+        hid: 'description',
+        name: 'description',
         content:
           "Passionné par la photographie, je propose mes services pour immortaliser tous types d'événements",
       },
-      { name: "format-detection", content: "telephone=no" },
+      { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ["~/assets/css/tailwind.css", "~/assets/css/transition.css"],
+  css: ['~/assets/css/tailwind.css', '~/assets/css/transition.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -34,8 +34,8 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
-    "@nuxtjs/tailwindcss",
-    "@nuxt/image",
+    '@nuxtjs/tailwindcss',
+    '@nuxt/image',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -43,4 +43,7 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
-};
+  env: {
+    apiUrl: process.env.API_URL || 'http://localhost:1337',
+  },
+}
